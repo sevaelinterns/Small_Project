81837c8ed944b7e4de02adc3ac2b0e27788bec6c
loki25072000 <lokesh1669010@gmail.com>
2023-02-06 16:38:58 +0530

    first change

--- src/app/master/master.component.ts
+++ src/app/master/master.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup,FormBuilder, Validators, } from '@angular/forms';
+import { FormGroup, FormBuilder, Validators, } from '@angular/forms';
 
 @Component({
   selector: 'app-master',
@@ -7,45 +7,44 @@ import { FormGroup,FormBuilder, Validators, } from '@angular/forms';
   styleUrls: ['./master.component.css']
 })
 export class MasterComponent implements OnInit {
-  studentForm:FormGroup;
-  constructor(private formbuilder:FormBuilder) {
+  studentForm: FormGroup;
+  constructor(private formbuilder: FormBuilder) {
     this.studentForm = this.formbuilder.group({
-      'name':[null,Validators.required],
-      'age':[null,Validators.required],
-      'gender':[null,Validators.required],
-      'education':[null,Validators.required],
-      'eligible':[null,Validators.required],
-      
+      'name': [null, Validators.required],
+      'age': [null, Validators.required],
+      'gender': [null, Validators.required],
+      'education': [null, Validators.required],
+      'eligible': [null, Validators.required],
+      status:false
+
     })
-   }
+  }
 
   ngOnInit() {
     this.getDetails()
   }
- stdArrays= new Array();
- stdObj:any;
- studentVlaues:any;
-submit(){
- this.studentVlaues= this.studentForm.value
- console.log(this.studentVlaues)
-// localStorage.setItem('studentsvalue',this.studentVlaues)
-localStorage.setItem('stdValues',JSON.stringify( this.studentVlaues))
-localStorage.getItem('stdValues')
-console.log(JSON.parse(localStorage.getItem('stdValues')))
+  stdArrays = new Array();
+  stdObj: any;
+  studentVlaues: any;
+  submit() {
+    this.studentVlaues = this.studentForm.value
 
-   this.getDetails()
-}
-getDetails(){
-  this. stdObj =   JSON.parse(localStorage.getItem('stdValues')) 
-  // this.stdArrays=[this.stdObj]
-  this.stdArrays.push(this.stdObj)
-  console.log(this.stdArrays)
-}
-// eligiable=false
-// changeValue(val){
-//   return this.eligiable=val
-// }
-change(data,status,e){
-  
-}
+    localStorage.setItem('stdValues', JSON.stringify(this.studentVlaues))
+    // localStorage.getItem('stdValues')
+    this.getDetails()
+  }
+  getDetails() {
+    this.stdObj = JSON.parse(localStorage.getItem('stdValues'))
+    this.stdArrays.push(this.stdObj)
+
+  }
+
+  change(value) {
+  console.log(value)
+  }
+  edit(val) {
+    console.log(val)
+    // this.studentForm=this.stdArrays[0]
+    this.studentForm.patchValue(val)
+  }
 }
